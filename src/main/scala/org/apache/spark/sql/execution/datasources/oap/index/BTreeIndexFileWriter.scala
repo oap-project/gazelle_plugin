1d0bde7322f8bfc49dc83fc17455b4f7f235dd1a
Liu, Linhong <linhong.liu@intel.com>
2017-10-27 04:34:21 -0500

    BTree Index Writer with Partial Loading (#345)
    * BTree Index Writer with Partial Loading
    
    * bug fix
    
    * address reviews
    
    * refine code

---
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BTreeIndexFileWriter.scala
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.oap.index
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.parquet.bytes.LittleEndianDataOutputStream
+
+import org.apache.spark.sql.execution.datasources.oap.io.IndexFile
+
+/**
+ * BTreeIndexFile Structure
+ * Field                          Description
+ * Version                        OAPIDX02
+ * Node #1                        Part #1 of all sorted key list
+ * Node #2
+ * ...
+ * Node #N
+ * Row ID List                    Row ID List sorted by corresponding key
+ * Footer                         Meta data for this index file
+ * Row Id List Section Size       4 bytes
+ * Footer Section Size            4 bytes
+ */
+private case class BTreeIndexFileWriter(
+    configuration: Configuration,
+    file: Path) {
+
+  private lazy val writer = file.getFileSystem(configuration).create(file, true)
+
+  private var rowIdListSize = 0
+  private var footerSize = 0
+  private def writeVersion(version: Int): Unit = {
+    writer.write("OAPIDX".getBytes("UTF-8"))
+    assert(version <= 65535)
+    val data = Array((version >> 8).toByte, (version & 0xFF).toByte)
+    writer.write(data)
+  }
+
+  def start(): Unit = {
+    writeVersion(IndexFile.INDEX_VERSION)
+  }
+
+  def writeNode(buf: Array[Byte]): Unit = {
+    writer.write(buf)
+  }
+
+  def writeRowIdList(buf: Array[Byte]): Unit = {
+    writer.write(buf)
+    rowIdListSize = buf.length
+  }
+
+  def writeFooter(footer: Array[Byte]): Unit = {
+    writer.write(footer)
+    footerSize = footer.length
+  }
+
+  def end(): Unit = {
+    val littleEndianWriter = new LittleEndianDataOutputStream(writer)
+    littleEndianWriter.writeInt(rowIdListSize)
+    littleEndianWriter.writeInt(footerSize)
+  }
+
+  def close(): Unit = writer.close()
+}
--- src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BTreeIndexRecordWriter.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BTreeIndexRecordWriter.scala
@@ -24,19 +24,22 @@ import scala.collection.JavaConverters._
 
 import com.google.common.collect.ArrayListMultimap
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FSDataOutputStream, Path}
 import org.apache.hadoop.mapreduce.{RecordWriter, TaskAttemptContext}
+import org.apache.parquet.bytes.LittleEndianDataOutputStream
 
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.expressions.codegen.GenerateOrdering
+import org.apache.spark.sql.execution.datasources.OapException
 import org.apache.spark.sql.execution.datasources.oap.io.IndexFile
 import org.apache.spark.sql.execution.datasources.oap.statistics.StatisticsManager
 import org.apache.spark.sql.execution.datasources.oap.utils.{BTreeNode, BTreeUtils}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types._
 
 private[index] class BTreeIndexRecordWriter(
     configuration: Configuration,
-    writer: OutputStream,
+    writer: FSDataOutputStream,
     keySchema: StructType) extends RecordWriter[Void, InternalRow] {
 
   @transient private lazy val genericProjector = FromUnsafeProjection(keySchema)
@@ -51,20 +54,23 @@ private[index] class BTreeIndexRecordWriter(
   }
 
   override def close(context: TaskAttemptContext): Unit = {
+    // TODO: Change to flush() after new reader is ready
     flushToFile(writer)
     writer.close()
   }
 
-  def flushToFile(out: OutputStream): Unit = {
-
+  private def sortUniqueKeys(): Array[InternalRow] = {
     def buildOrdering(keySchema: StructType): Ordering[InternalRow] = {
       // here i change to use param id to index_id to get datatype in keySchema
       val order = keySchema.zipWithIndex.map {
         case (field, index) => SortOrder(
           BoundReference(index, field.dataType, nullable = true),
-          if (field.metadata.getBoolean("isAscending")) Ascending else Descending)
+          if (!field.metadata.contains("isAscending") || field.metadata.getBoolean("isAscending")) {
+            Ascending
+          } else {
+            Descending
+          })
       }
-
       GenerateOrdering.generate(order, keySchema.toAttributes)
     }
 
@@ -87,6 +93,13 @@ private[index] class BTreeIndexRecordWriter(
     }
     // sort keys
     java.util.Arrays.sort(uniqueKeys, comparator)
+    uniqueKeys
+  }
+
+  def flushToFile(out: OutputStream): Unit = {
+
+    val uniqueKeys = sortUniqueKeys()
+    val partitionUniqueSize = uniqueKeys.length
     // build index file
     var i = 0
     var fileOffset = 0L
@@ -230,4 +243,183 @@ private[index] class BTreeIndexRecordWriter(
     writer.write(data)
     IndexFile.indexFileHeaderLength
   }
+
+  // TODO: Pass index file Path
+  private val fileWriter = BTreeIndexFileWriter(configuration, new Path("./temp"))
+
+  /**
+   * Working Flow:
+   *  1. Call fileWriter.start() to write some Index Info
+   *  2. Split all unique keys into some nodes
+   *  3. Serialize nodes and call fileWriter.writeNode()
+   *  4. Serialize row id List based on sorted unique keys and call fileWriter.writeRowIdList()
+   *  5. Serialize footer and call fileWriter.writeFooter()
+   *  5. Call fileWriter.end() to write some meta data (e.g. file offset for each section)
+   */
+  private def flush(): Unit = {
+    val uniqueKeys = sortUniqueKeys()
+    val treeShape = BTreeUtils.generate2(uniqueKeys.length)
+    // Trick here. If root node has no child, then write root node as a child.
+    val children = if (treeShape.children.nonEmpty) treeShape.children else treeShape :: Nil
+
+    // Start
+    fileWriter.start()
+    // Write Node
+    var startPosInKeyList = 0
+    var startPosInRowList = 0
+    val nodes =
+      children.map { node =>
+        val keyCount = sumKeyCount(node)
+        val nodeUniqueKeys = uniqueKeys.slice(startPosInKeyList, startPosInKeyList + keyCount)
+        val rowCount = nodeUniqueKeys.map(multiHashMap.get(_).size()).sum
+
+        val nodeBuf = serializeNode(nodeUniqueKeys, startPosInRowList)
+        fileWriter.writeNode(nodeBuf)
+        startPosInKeyList += keyCount
+        startPosInRowList += rowCount
+        BTreeNodeMetaData(rowCount, nodeBuf.length, nodeUniqueKeys.head, nodeUniqueKeys.last)
+      }
+    // Write Row Id List
+    fileWriter.writeRowIdList(serializeRowIdLists(uniqueKeys))
+    // Write Footer
+    fileWriter.writeFooter(serializeFooter(nodes))
+    // End
+    fileWriter.end()
+  }
+
+  /**
+   * Layout of node:
+   * Field Description                        Byte Size
+   * Key Meta Section                         4 + 4 * N
+   * Key Count                                 4 Bytes
+   * Start Pos for Key #1 in Key Data          4 Bytes
+   * Start Pos for Key #1 in Row Id List       4 Bytes
+   * ...
+   * Start Pos for Key #N in Key Data          4 Bytes
+   * Start Pos for Key #N in Row Id List       4 Bytes
+   * Key Data Section                         M Bytes
+   * Key Data For Key #1
+   * ...
+   * Key Data For Key #N
+   */
+  private def serializeNode(uniqueKeys: Array[InternalRow], startPosInRowList: Int): Array[Byte] = {
+    val buffer = new ByteArrayOutputStream()
+    val output = new LittleEndianDataOutputStream(buffer)
+    val keyBuffer = new ByteArrayOutputStream()
+    val keyOutput = new LittleEndianDataOutputStream(keyBuffer)
+
+    output.writeInt(uniqueKeys.length)
+    var rowPos = startPosInRowList
+    uniqueKeys.foreach { key =>
+      output.writeInt(keyBuffer.size())
+      output.writeInt(rowPos)
+      writeBasedOnSchema(keyOutput, key, keySchema)
+      rowPos += multiHashMap.get(key).size()
+    }
+    buffer.toByteArray ++ keyBuffer.toByteArray
+  }
+
+  // TODO: BTreeNode can be re-write. It doesn't carry any values.
+  private def sumKeyCount(node: BTreeNode): Int = {
+    if (node.children.nonEmpty) node.children.map(sumKeyCount).sum
+    else node.root
+  }
+
+  /**
+   * Example of Row Id List:
+   * Row Id: 0 1 2 3 4 5 6 7 8 9
+   * Key:    1 2 3 4 1 2 3 4 1 2
+   * Then Row Id List is Stored as: 0481592637
+   */
+  private def serializeRowIdLists(uniqueKeys: Array[InternalRow]): Array[Byte] = {
+    val buffer = new ByteArrayOutputStream()
+    val out = new LittleEndianDataOutputStream(buffer)
+    uniqueKeys.flatMap(key => multiHashMap.get(key).asScala).foreach(out.writeInt)
+    buffer.toByteArray
+  }
+
+  /**
+   * Layout of Footer:
+   * Field Description              Byte Size
+   * Row Count                      4 Bytes
+   * Node Count                     4 Bytes
+   * Nodes Meta Data                Node Count * 20 Bytes
+   * Row Count                        4 Bytes
+   * Start Pos                        4 Bytes
+   * Size In Byte                     4 Bytes
+   * Min Key Pos in Key Data          4 Bytes
+   * Max Key Pos in Key Data          4 Bytes
+   * Key Data - Variable Bytes      M
+   * Min Key For Child #1 - Min
+   * Max Key For Child #1
+   * ...
+   * Min Key For Child #N
+   * Max Key For Child #N - Max
+   * TODO: Make serialize and deserialize(in reader) in same style.
+   */
+  private def serializeFooter(nodes: Seq[BTreeNodeMetaData]): Array[Byte] = {
+    val buffer = new ByteArrayOutputStream()
+    val output = new LittleEndianDataOutputStream(buffer)
+
+    val keyBuffer = new ByteArrayOutputStream()
+    val keyOutput = new LittleEndianDataOutputStream(keyBuffer)
+
+    // Record Count
+    output.writeInt(nodes.map(_.rowCount).sum)
+    // Child Count
+    output.writeInt(nodes.size)
+
+    var offset = 0
+    nodes.foreach { node =>
+      // Row Count for each Child
+      output.writeInt(node.rowCount)
+      // Start Pos for each Child
+      output.writeInt(offset)
+      // Size for each Child
+      output.writeInt(node.byteSize)
+      // Min Key Pos for each Child
+      output.writeInt(keyBuffer.size())
+      writeBasedOnSchema(keyOutput, node.min, keySchema)
+      // Max Key Pos for each Child
+      output.writeInt(keyBuffer.size())
+      writeBasedOnSchema(keyOutput, node.max, keySchema)
+      offset += node.byteSize
+    }
+    buffer.toByteArray ++ keyBuffer.toByteArray
+  }
+
+  private def writeBasedOnSchema(
+      writer: LittleEndianDataOutputStream, row: InternalRow, schema: StructType): Unit = {
+    schema.zipWithIndex.foreach {
+      case (field, index) => writeBasedOnDataType(writer, row, field.dataType, index)
+    }
+  }
+
+  private def writeBasedOnDataType(
+      writer: LittleEndianDataOutputStream,
+      row: InternalRow,
+      dataType: DataType,
+      ordinal: Int): Unit = {
+    dataType match {
+      case BooleanType => writer.writeBoolean(row.getBoolean(ordinal))
+      case ByteType => writer.writeByte(row.getByte(ordinal))
+      case ShortType => writer.writeShort(row.getShort(ordinal))
+      case IntegerType => writer.writeInt(row.getInt(ordinal))
+      case LongType => writer.writeLong(row.getLong(ordinal))
+      case FloatType => writer.writeFloat(row.getFloat(ordinal))
+      case DoubleType => writer.writeDouble(row.getDouble(ordinal))
+      case DateType => writer.writeInt(row.getInt(ordinal))
+      case StringType =>
+        val bytes = row.getUTF8String(ordinal).getBytes
+        writer.writeInt(bytes.length)
+        writer.write(bytes)
+      case BinaryType =>
+        val bytes = row.getBinary(ordinal)
+        writer.writeInt(bytes.length)
+        writer.write(bytes)
+      case _ => throw new OapException("not support data type")
+    }
+  }
 }
+private case class BTreeNodeMetaData(
+    rowCount: Int, byteSize: Int, min: InternalRow, max: InternalRow)
