c2741be8ad1e49c5d10310e8b693894fda09833a
Daoyuan Wang <daoyuan.wang@intel.com>
2018-02-26 16:08:39 +0800

    disable index selection in filtersuite (#593)
    * disable index selection in filtersuite
    
    * address comments

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/FilterSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/FilterSuite.scala
@@ -35,6 +35,19 @@ class FilterSuite extends QueryTest with SharedOapContext with BeforeAndAfterEac
   import testImplicits._
 
   private var currentPath: String = _
+  private var defaultEis: Boolean = true
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    // In this suite we don't want to skip index even if the cost is higher.
+    defaultEis = sqlConf.getConf(OapConf.OAP_ENABLE_EXECUTOR_INDEX_SELECTION)
+    sqlConf.setConf(OapConf.OAP_ENABLE_EXECUTOR_INDEX_SELECTION, false)
+  }
+
+  override def afterAll(): Unit = {
+    sqlConf.setConf(OapConf.OAP_ENABLE_EXECUTOR_INDEX_SELECTION, defaultEis)
+    super.afterAll()
+  }
 
   override def beforeEach(): Unit = {
     val path = Utils.createTempDir().getAbsolutePath
