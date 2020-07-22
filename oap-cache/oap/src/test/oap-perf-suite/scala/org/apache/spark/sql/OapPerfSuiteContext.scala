cbdc691d1aa4c68651c84cc9418469c06d3f17da
Hong <hong2.wang@intel.com>
2020-07-22 18:25:01 +0800

    [OAP-1512][sql index and data source cache] Fix oap-perf-suite to sync with SparkEnv.scala decouple (#1514)

--- oap-cache/oap/src/test/oap-perf-suite/scala/org/apache/spark/sql/OapPerfSuiteContext.scala
+++ oap-cache/oap/src/test/oap-perf-suite/scala/org/apache/spark/sql/OapPerfSuiteContext.scala
@@ -22,6 +22,7 @@ package org.apache.spark.sql
 
 import sys.process._
 import org.apache.spark._
+import org.apache.spark.sql.oap.OapRuntime
 
 trait OapPerfSuiteContext {
 
@@ -82,6 +83,7 @@ trait OapPerfSuiteContext {
   def afterAll(): Unit = {
     if (_spark != null) {
       SparkSession.clearActiveSession()
+      OapRuntime.stop()
       _spark.stop()
       _spark = null
       assert(("rm -f ./metastore_db/db.lck" !) == 0)
