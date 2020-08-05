399ac070df5a0c443f1e26b8fc12e5840ceaa66e
Xiaochang Wu <xiaochang.wu@intel.com>
2020-08-05 15:25:42 +0800

    [OAP-1562][OAP-MLlib] Fix CI for oneapi beta07 (#1563)
    * adjust path to fix travis for oneapi beta07
    
    * add ONEAPI_ROOT
    
    * fix for centos

--- oap-mllib/dev/install-build-deps-bionic.sh
+++ oap-mllib/dev/install-build-deps-bionic.sh
@@ -7,11 +7,12 @@ sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update
-sudo apt-get install intel-oneapi-daal-devel intel-oneapi-tbb-devel
+sudo apt-get install intel-oneapi-daal-devel-2021.1-beta07 intel-oneapi-tbb-devel-2021.1-beta07
 
 echo "Building oneCCL ..."
 cd /tmp
 git clone https://github.com/oneapi-src/oneCCL
+git checkout -b 2021.1-beta07-1 origin/2021.1-beta07-1
 cd oneCCL && mkdir build && cd build
 cmake ..
 make -j 2 install
--- oap-mllib/dev/install-build-deps-centos.sh
+++ oap-mllib/dev/install-build-deps-centos.sh
@@ -12,11 +12,12 @@ repo_gpgcheck=1
 gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 EOF
 sudo mv /tmp/oneAPI.repo /etc/yum.repos.d
-sudo yum install intel-oneapi-daal-devel intel-oneapi-tbb-devel
+sudo yum install intel-oneapi-daal-devel-2021.1-beta07 intel-oneapi-tbb-devel-2021.1-beta07
 
 echo "Building oneCCL ..."
 cd /tmp
 git clone https://github.com/oneapi-src/oneCCL
+git checkout -b 2021.1-beta07-1 origin/2021.1-beta07-1
 cd oneCCL && mkdir build && cd build
 cmake ..
 make -j 2 install
