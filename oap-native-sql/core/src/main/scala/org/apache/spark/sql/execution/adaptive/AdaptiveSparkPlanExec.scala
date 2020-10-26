0fc2e4f78375ddc38556cb98b297cb30490a5a77
Yuan <yuan.zhou@intel.com>
2020-10-26 13:30:48 +0800

    [Scala] fix CoalesceBatchs after HashAgg (#1834)
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- oap-native-sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AdaptiveSparkPlanExec.scala
+++ oap-native-sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AdaptiveSparkPlanExec.scala
@@ -404,7 +404,9 @@ case class AdaptiveSparkPlanExec(
         if (columnarBHJEnabled && parent.isInstanceOf[BroadcastHashJoinExec]) {
           val columnarExchangeChild = b.child match {
             case WholeStageCodegenExec(child: ColumnarToRowExec) =>
-              CoalesceBatchesExec(child.child)
+              //CoalesceBatchesExec(child.child)
+              //TODO:() enable this when fixing hashagg hasNext
+              child.child
             case child: ColumnarToRowExec =>
               CoalesceBatchesExec(child.child)
             case child => {
