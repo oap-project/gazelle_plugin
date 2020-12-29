cea3f04cc8a909094eeb811cb0319076d0200f92
zhixingheyi-tian  <xiangxiang.shen@intel.com>
2020-12-29 15:21:42 +0800

    [NATIVE-SQL-ENGINE-1] Make compiling succeed

---
+++ cpp/compile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+echo $CURRENT_DIR
+
+cd ${CURRENT_DIR}
+if [ -d build ]; then
+    rm -r build
+fi
+mkdir build
+cd build
+cmake ..
+make
+
+set +eu
+
