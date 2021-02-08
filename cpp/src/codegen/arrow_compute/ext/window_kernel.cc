1ca09c991d44a6a47cd4f6fc97f9cdb7567f949e
Hongze Zhang <hongze.zhang@intel.com>
2021-02-08 12:46:04 +0800

    [NSE-101] ColumnarWindow: Remove obsolete debug code

--- cpp/src/codegen/arrow_compute/ext/window_kernel.cc
+++ cpp/src/codegen/arrow_compute/ext/window_kernel.cc
@@ -388,9 +388,6 @@ static arrow::Status DecodeIndices(std::shared_ptr<arrow::Array> in, std::vector
 }
 
 arrow::Status WindowRankKernel::SortToIndicesPrepare(std::vector<ArrayList> values) {
-#ifdef DEBUG
-  std::cout << "RANK: values to sort: " << values.at(0)->ToString() << std::endl;
-#endif
   for (auto each_batch : values) {
     RETURN_NOT_OK(sorter_->Evaluate(each_batch));
   }
@@ -400,9 +397,6 @@ arrow::Status WindowRankKernel::SortToIndicesPrepare(std::vector<ArrayList> valu
 
 arrow::Status WindowRankKernel::SortToIndicesFinish(std::vector<std::shared_ptr<ArrayItemIndex>> elements_to_sort,
                                                     std::vector<std::shared_ptr<ArrayItemIndex>> *offsets) {
-#ifdef DEBUG
-  std::cout << "RANK: partition: " << elements_to_sort->ToString() << std::endl;
-#endif
   std::shared_ptr<arrow::Array> in;
   std::shared_ptr<arrow::Array> out;
   RETURN_NOT_OK(EncodeIndices(elements_to_sort, &in));
