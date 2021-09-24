4eb785a8b584ced327c435824b3aa7252cea3804
Chendi Xue <chendi.xue@intel.com>
2021-09-24 11:04:34 +0800

    Use integrated arrow stream writer and reader api to replace original spill and read codes
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- native-sql-engine/cpp/src/codegen/arrow_compute/ext/spillable_cache_store.h
+++ native-sql-engine/cpp/src/codegen/arrow_compute/ext/spillable_cache_store.h
@@ -41,6 +41,7 @@ namespace codegen {
 namespace arrowcompute {
 namespace extra {
 using ArrayList = std::vector<std::shared_ptr<arrow::Array>>;
+static constexpr int32_t kIpcContinuationToken = -1;
 
 // TODO: move this class into common utility
 class SpillableCacheStore {
@@ -61,7 +62,7 @@ class SpillableCacheStore {
       std::shared_ptr<ResultIterator<arrow::RecordBatch>> iter, int64_t* spilled_size,
       std::shared_ptr<ResultIterator<arrow::RecordBatch>>* out) {
     RETURN_NOT_OK(DoSpill(iter, spilled_size));
-    *out = std::make_shared<SpillableCacheReaderIterator>(spill_file_list_);
+    *out = std::make_shared<SpillableCacheReaderIterator>(spill_file_list_, num_batches_);
     return arrow::Status::OK();
   }
 
@@ -77,15 +78,28 @@ class SpillableCacheStore {
       RETURN_NOT_OK(fs->CreateDir(local_spill_dir_, true));
     }
 
+    std::string spilled_file = local_spill_dir_ + "/0.arrow";  // TODO(): get tmp dir
+    auto spilled_file_os = *fs->OpenOutputStream(spilled_file);
+
     int64_t size = 0;
     int i = 0;
+    bool first_batch = true;
+    std::shared_ptr<arrow::ipc::RecordBatchWriter> writer;
+
     while (iter->HasNext()) {
       int64_t single_call_spilled;
       std::shared_ptr<arrow::RecordBatch> batch;
       RETURN_NOT_OK(iter->Next(&batch));
-      RETURN_NOT_OK(SpillData(i++, batch, &single_call_spilled));
-      *spilled_size += single_call_spilled;
+      if (first_batch) {
+        writer = *arrow::ipc::MakeStreamWriter(spilled_file_os.get(), batch->schema());
+        first_batch = false;
+      }
+      RETURN_NOT_OK(writer->WriteRecordBatch(*batch));
+      num_batches_++;
     }
+    RETURN_NOT_OK(spilled_file_os->Flush());
+    RETURN_NOT_OK(writer->Close());
+    spill_file_list_.push_back(spilled_file);
     SetSpilled(true);
     return arrow::Status::OK();
   }
@@ -113,38 +127,11 @@ class SpillableCacheStore {
     return str + ".arrow";
   }
 
-  arrow::Status SpillData(int64_t idx, std::shared_ptr<arrow::RecordBatch> batch,
-                          int64_t* spilled_size) {
-    arrow::ipc::IpcPayload payload;
-    arrow::ipc::IpcWriteOptions options_;
-    if (!result_schema_) result_schema_ = batch->schema();
-
-    arrow::ipc::DictionaryFieldMapper dict_file_mapper;  // unused
-    std::shared_ptr<arrow::io::FileOutputStream> spilled_file_os_;
-    std::string spilled_file_ =
-        local_spill_dir_ + "/" + std::to_string(idx) + ".arrow";  // TODO(): get tmp dir
-
-    ARROW_ASSIGN_OR_RAISE(spilled_file_os_,
-                          arrow::io::FileOutputStream::Open(spilled_file_, true));
-    int32_t metadata_length = -1;
-    auto schema_payload_ = std::make_shared<arrow::ipc::IpcPayload>();
-    RETURN_NOT_OK(arrow::ipc::GetSchemaPayload(*result_schema_.get(), options_,
-                                               dict_file_mapper, schema_payload_.get()));
-    ARROW_RETURN_NOT_OK(arrow::ipc::WriteIpcPayload(
-        *schema_payload_.get(), options_, spilled_file_os_.get(), &metadata_length));
-    int32_t data_length = -1;
-    arrow::ipc::GetRecordBatchPayload(*batch, options_, &payload);
-    ARROW_RETURN_NOT_OK(arrow::ipc::WriteIpcPayload(
-        payload, options_, spilled_file_os_.get(), &data_length));
-    *spilled_size = data_length;
-    spill_file_list_.push_back(spilled_file_);
-    return arrow::Status::OK();
-  }
-
   std::vector<arrow::ArrayVector> GetCache() {
     if (!cached_.empty()) return cached_;
     if (is_spilled_) {
-      auto iter = std::make_shared<SpillableCacheReaderIterator>(spill_file_list_);
+      auto iter =
+          std::make_shared<SpillableCacheReaderIterator>(spill_file_list_, num_batches_);
       while (iter->HasNext()) {
         std::shared_ptr<arrow::RecordBatch> out;
         iter->Next(&out);
@@ -162,35 +149,27 @@ class SpillableCacheStore {
 
   class SpillableCacheReaderIterator : public ResultIterator<arrow::RecordBatch> {
    public:
-    SpillableCacheReaderIterator(std::vector<std::string> spill_file_list)
-        : spill_file_list_(spill_file_list) {}
-
-    arrow::Result<std::shared_ptr<arrow::ipc::RecordBatchReader>>
-    GetRecordBatchStreamReader(const std::string& file_name) {
-      ARROW_ASSIGN_OR_RAISE(auto file_, arrow::io::ReadableFile::Open(file_name))
-      ARROW_ASSIGN_OR_RAISE(auto file_reader,
-                            arrow::ipc::RecordBatchStreamReader::Open(file_));
-      return file_reader;
+    SpillableCacheReaderIterator(std::vector<std::string> spill_file_list,
+                                 const int& num_batches)
+        : spill_file_list_(spill_file_list), total_num_batches_(num_batches) {
+      auto file = *arrow::io::ReadableFile::Open(spill_file_list_[0]);
+      file_reader_ = *arrow::ipc::RecordBatchStreamReader::Open(
+          file, arrow::ipc::IpcReadOptions::Defaults());
     }
 
-    bool HasNext() override { return cur_file_idx_ < spill_file_list_.size(); }
+    bool HasNext() override { return cur_idx_ < total_num_batches_; }
 
     arrow::Status Next(std::shared_ptr<arrow::RecordBatch>* out) override {
-#ifdef DEBUG
-      std::cout << "read from " << spill_file_list_[cur_file_idx_] << std::endl;
-#endif
-      ARROW_ASSIGN_OR_RAISE(auto file_reader, GetRecordBatchStreamReader(
-                                                  spill_file_list_[cur_file_idx_++]));
-
-      std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
-      RETURN_NOT_OK(file_reader->ReadAll(&batches));
-      *out = batches[0];
+      RETURN_NOT_OK(file_reader_->ReadNext(out));
+      cur_idx_++;
       return arrow::Status::OK();
     }
 
    private:
     std::vector<std::string> spill_file_list_;
-    int cur_file_idx_ = 0;
+    int cur_idx_ = 0;
+    std::shared_ptr<arrow::ipc::RecordBatchStreamReader> file_reader_;
+    const int& total_num_batches_ = 0;
   };
 
  private:
@@ -199,6 +178,8 @@ class SpillableCacheStore {
   bool is_spilled_ = false;
   std::string local_spill_dir_;
   std::vector<arrow::ArrayVector> cached_;
+  arrow::ipc::IpcWriteOptions options_;
+  int num_batches_ = 0;
 };
 
 }  // namespace extra
