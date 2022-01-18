e5ba3d5725a48d41f9ad72a279275ec48ff74582
Rui Mo <rui.mo@intel.com>
2022-01-18 16:27:25 +0800

    add back ArrayType

--- native-sql-engine/core/src/main/scala/com/intel/oap/execution/ArrowRowToColumnarExec.scala
+++ native-sql-engine/core/src/main/scala/com/intel/oap/execution/ArrowRowToColumnarExec.scala
@@ -63,6 +63,7 @@ class ArrowRowToColumnarExec(child: SparkPlan) extends RowToColumnarExec(child =
         case d: DecimalType =>
         case d: TimestampType =>
         case d: BinaryType =>
+        case d: ArrayType =>
         case _ =>
           throw new UnsupportedOperationException(s"${field.dataType} " +
             s"is not supported in ArrowRowToColumnarExec.")
