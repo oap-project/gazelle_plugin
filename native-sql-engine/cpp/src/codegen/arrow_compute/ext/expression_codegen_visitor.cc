4cfe29aba2919da5522cc88f23e326dc71c4751b
PHILO-HE <feilong.he@intel.com>
2022-02-28 07:34:42 +0800

    Catch out_of_range exception (#743)

--- native-sql-engine/cpp/src/codegen/arrow_compute/ext/expression_codegen_visitor.cc
+++ native-sql-engine/cpp/src/codegen/arrow_compute/ext/expression_codegen_visitor.cc
@@ -683,6 +683,8 @@ arrow::Status ExpressionCodegenVisitor::Visit(const gandiva::FunctionNode& node)
                << ");" << std::endl;
     prepare_ss << "} catch (std::invalid_argument) {" << std::endl;
     prepare_ss << validity << " = false;" << std::endl;
+    prepare_ss << "} catch (std::out_of_range)" << std::endl;
+    prepare_ss << validity << " = false;" << std::endl;
     prepare_ss << "}" << std::endl;
     prepare_ss << "}" << std::endl;
 
