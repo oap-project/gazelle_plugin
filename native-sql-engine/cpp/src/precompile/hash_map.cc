181c7b5296ad143e7f1bf50dd046e2d91e6a1864
Yuan Zhou <yuan.zhou@intel.com>
2022-04-20 18:01:27 +0800

    improve hashmap
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- native-sql-engine/cpp/src/precompile/hash_map.cc
+++ native-sql-engine/cpp/src/precompile/hash_map.cc
@@ -27,26 +27,6 @@
 namespace sparkcolumnarplugin {
 namespace precompile {
 
-#define TYPED_SPARSE_HASH_MAP_IMPL(TYPENAME, TYPE)                                      \
-  class TYPENAME::Impl : public SparseHashMap<TYPE> {                                   \
-   public:                                                                              \
-    Impl(arrow::MemoryPool* pool) : SparseHashMap<TYPE>(pool) {}                        \
-  };                                                                                    \
-                                                                                        \
-  TYPENAME::TYPENAME(arrow::MemoryPool* pool) { impl_ = std::make_shared<Impl>(pool); } \
-  arrow::Status TYPENAME::GetOrInsert(const TYPE& value, void (*on_found)(int32_t),     \
-                                      void (*on_not_found)(int32_t),                    \
-                                      int32_t* out_memo_index) {                        \
-    return impl_->GetOrInsert(value, on_found, on_not_found, out_memo_index);           \
-  }                                                                                     \
-  int32_t TYPENAME::GetOrInsertNull(void (*on_found)(int32_t),                          \
-                                    void (*on_not_found)(int32_t)) {                    \
-    return impl_->GetOrInsertNull(on_found, on_not_found);                              \
-  }                                                                                     \
-  int32_t TYPENAME::Get(const TYPE& value) { return impl_->Get(value); }                \
-  int32_t TYPENAME::GetNull() { return impl_->GetNull(); }
-
-#undef TYPED_SPARSE_HASH_MAP_IMPL
 
 #define TYPED_ARROW_HASH_MAP_IMPL(HASHMAPNAME, TYPENAME, TYPE, MEMOTABLETYPE)          \
   using MEMOTABLETYPE =                                                                \
