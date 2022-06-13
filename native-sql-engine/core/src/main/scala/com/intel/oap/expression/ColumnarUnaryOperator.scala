f63de9d7dd4010cf319988efa2ca243c688ddb8b
philo <feilong.he@intel.com>
2022-06-13 23:28:08 +0800

    Initial commit

--- native-sql-engine/core/src/main/scala/com/intel/oap/expression/ColumnarUnaryOperator.scala
+++ native-sql-engine/core/src/main/scala/com/intel/oap/expression/ColumnarUnaryOperator.scala
@@ -923,6 +923,22 @@ class ColumnarLength(child: Expression) extends Length(child: Expression)
   }
 }
 
+class ColumnarHex(child: Expression) extends Hex(child: Expression)
+  with ColumnarExpression with Logging {
+
+  override def supportColumnarCodegen(args: java.lang.Object): Boolean = {
+    false
+  }
+
+  override def doColumnarCodeGen(args: java.lang.Object): (TreeNode, ArrowType) = {
+    val (child_node, _): (TreeNode, ArrowType) =
+      child.asInstanceOf[ColumnarExpression].doColumnarCodeGen(args)
+    val resultType = new ArrowType.Utf8()
+    (TreeBuilder.makeFunction("hex", Lists.newArrayList(child_node),
+      resultType), resultType)
+  }
+}
+
 object ColumnarUnaryOperator {
 
   def create(child: Expression, original: Expression): Expression = original match {
@@ -992,6 +1008,8 @@ object ColumnarUnaryOperator {
       new ColumnarRand(child)
     case len: Length =>
       new ColumnarLength(child)
+    case hex: Hex =>
+      new ColumnarHex(child)
     case other =>
       child.dataType match {
         case _: DateType => other match {
