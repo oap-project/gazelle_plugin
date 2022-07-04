e9c8d7965d1a2922a4a0bdd7eee37b027601e028
philo <feilong.he@intel.com>
2022-07-04 23:31:16 +0800

    Handle different input types

--- native-sql-engine/core/src/main/scala/com/intel/oap/expression/ColumnarHashExpression.scala
+++ native-sql-engine/core/src/main/scala/com/intel/oap/expression/ColumnarHashExpression.scala
@@ -23,26 +23,43 @@ import org.apache.arrow.vector.types.pojo.ArrowType
 
 import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.Murmur3Hash
+import org.apache.spark.sql.types._
 
 
 class ColumnarMurmur3Hash(children: Seq[Expression], seed: Int)
   extends Murmur3Hash(children: Seq[Expression], seed: Int) with ColumnarExpression {
+
+  buildCheck
+
+  def buildCheck(): Unit = {
+    val supportedTypes =
+      List(ByteType, ShortType, IntegerType, LongType, FloatType, DoubleType, StringType)
+    if (children.map(child => supportedTypes.contains(child.dataType)).exists(_ == false)) {
+      throw new UnsupportedOperationException("Fall back hash expression because" +
+        " there is at least one unsupported input type!")
+    }
+  }
 
   override def supportColumnarCodegen(args: java.lang.Object): Boolean = {
     false
   }
 
   override def doColumnarCodeGen(args: Object): (TreeNode, ArrowType) = {
-
     val resultType = new ArrowType.Int(32, true)
     var hashNode = TreeBuilder.makeLiteral(new Integer(seed))
     var funcNode: TreeNode = null
     for (child: Expression <- children) {
       val (childNode, _): (TreeNode, ArrowType) =
         child.asInstanceOf[ColumnarExpression].doColumnarCodeGen(args)
+      val gandivaFuncName = child.dataType match {
+        case LongType => "hash64_spark"
+        case DoubleType => "hash64_spark"
+        case StringType => "hashbuf_spark"
+        case _ => "hash32_spark"
+      }
       funcNode =
         TreeBuilder.makeFunction(
-          "hash32_spark",
+          gandivaFuncName,
           Lists.newArrayList(childNode, hashNode),
           resultType)
       hashNode = funcNode
