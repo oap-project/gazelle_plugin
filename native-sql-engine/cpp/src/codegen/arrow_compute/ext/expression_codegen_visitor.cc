f3cea25b657572cad957f9ed748b210aa10d63a0
philo <feilong.he@intel.com>
2022-09-21 20:34:40 +0800

    Fix codegen issue in locate (instr)

--- native-sql-engine/cpp/src/codegen/arrow_compute/ext/expression_codegen_visitor.cc
+++ native-sql-engine/cpp/src/codegen/arrow_compute/ext/expression_codegen_visitor.cc
@@ -335,8 +335,9 @@ arrow::Status ExpressionCodegenVisitor::Visit(const gandiva::FunctionNode& node)
     prepare_ss << "bool " << validity << " = " << child_visitor_list[0]->GetPreCheck()
                << ";" << std::endl;
     prepare_ss << "if (" << validity << ") {" << std::endl;
-    prepare_ss << "auto ind = " << child_visitor_list[0]->GetResult() << ".find("
-               << child_visitor_list[1]->GetResult() << ");" << std::endl;
+    // The 1st arg is the string we are searching for inside the 2nd arg.
+    prepare_ss << "auto ind = " << child_visitor_list[1]->GetResult() << ".find("
+               << child_visitor_list[0]->GetResult() << ");" << std::endl;
     prepare_ss << "if (ind == std::string::npos) {" << std::endl;
     prepare_ss << codes_str_ << " = 0;" << std::endl;
     prepare_ss << "}" << std::endl;
@@ -344,7 +345,7 @@ arrow::Status ExpressionCodegenVisitor::Visit(const gandiva::FunctionNode& node)
     prepare_ss << codes_str_ << " = ind + 1;" << std::endl;
     prepare_ss << "}" << std::endl;
     prepare_ss << "}" << std::endl;
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 2; i++) {
       prepare_str_ += child_visitor_list[i]->GetPrepare();
     }
     prepare_str_ += prepare_ss.str();
